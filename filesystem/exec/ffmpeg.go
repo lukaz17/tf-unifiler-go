f3f29141d8ffbbf595da3c09193166829b61c835
Lukaz v Precision <30133485+lukazvprecisn@users.noreply.github.com>
2024-09-03 11:51:40 +0700

    perf: apply filter to improve quality for HDR video screenshot

--- filesystem/exec/ffmpeg.go
+++ filesystem/exec/ffmpeg.go
@@ -18,25 +18,29 @@ type FFmpegArgsOptions struct {
 	OutputFrameCount nullable.Int
 	OutputStartTime  nullable.Int
 	QualityFactor    nullable.Int
+	VideoFilter      string
 	OverwiteOutput   bool
 }
 
 func (args FFmpegArgs) Compile() []string {
 	results := []string{}
 	if args.Options.InputStartTime.IsValid {
-		results = append(results, []string{"-ss", strconv.Itoa(args.Options.InputStartTime.RealValue)}...)
+		results = append(results, "-ss", strconv.Itoa(args.Options.InputStartTime.RealValue))
 	}
 	if args.Options.InputFile != "" {
-		results = append(results, []string{"-i", args.Options.InputFile}...)
+		results = append(results, "-i", args.Options.InputFile)
 	}
 	if args.Options.OutputStartTime.IsValid {
-		results = append(results, []string{"-ss", strconv.Itoa(args.Options.OutputStartTime.RealValue)}...)
+		results = append(results, "-ss", strconv.Itoa(args.Options.OutputStartTime.RealValue))
 	}
 	if args.Options.OutputFrameCount.IsValid {
-		results = append(results, []string{"-frames", strconv.Itoa(args.Options.OutputFrameCount.RealValue)}...)
+		results = append(results, "-frames", strconv.Itoa(args.Options.OutputFrameCount.RealValue))
 	}
 	if args.Options.QualityFactor.IsValid {
-		results = append(results, []string{"-q", strconv.Itoa(args.Options.QualityFactor.RealValue)}...)
+		results = append(results, "-q", strconv.Itoa(args.Options.QualityFactor.RealValue))
+	}
+	if args.Options.VideoFilter != "" {
+		results = append(results, "-vf", args.Options.VideoFilter)
 	}
 	if args.Options.OverwiteOutput {
 		results = append(results, "-y")
